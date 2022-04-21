ac15bf5c8dfa8a6e4a302209dd76516fbf48dffd
Benjamin Marty <bmarty@kochag.ch>
2022-04-21 12:53:27 +0200

    modify for i2p mode

--- videodecoder/videodecoder_yuyv.c
+++ videodecoder/videodecoder_yuyv.c
@@ -17,23 +17,13 @@ void decode_init() {
 }
 
 char *decode_frame(char* data, uint32_t frame_number) {
-    if(frame_number % 2 == 0) {
-        if(data == NULL)    return;
+    if(data == NULL)    return;
 
-        for(int y = 1; y < (CANVAS_HEIGHT/2); y+=1) {
-            memcpy(uyvybuffer + ( ( (y * 2) - 1) * CANVAS_WIDTH * 2), data + (y * IMAGE_WIDTH * 2), IMAGE_WIDTH * 2);
-        }
-
-        return uyvybuffer;
-    } else {
-        if(data == NULL)    return;
-
-        for(int y = 1; y < (CANVAS_HEIGHT/2); y+=1) {
-            memcpy(uyvybuffer +           ( y* 2 * CANVAS_WIDTH * 2), data + (y * IMAGE_WIDTH * 2), IMAGE_WIDTH * 2);
-        }
-        
-        return uyvybuffer;
+    for(int y = 1; y < IMAGE_HEIGHT; y++) {
+        memcpy(uyvybuffer + (y * CANVAS_WIDTH * 2), data  + (y * IMAGE_WIDTH * 2), IMAGE_WIDTH * 2);
     }
+
+    return uyvybuffer;
 }
 
 #endif
